27a1812e64aa116beff5292bf141d84610310ab6
Jack Wong <jackwong@professional.com>
2021-03-05 23:56:33 -0500

    Initial Commit

---
+++ Shared/King_s_CupApp.swift
@@ -0,0 +1,17 @@
+//
+//  King_s_CupApp.swift
+//  Shared
+//
+//  Created by Jack Wong on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct King_s_CupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
